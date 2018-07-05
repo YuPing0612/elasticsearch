798368c7a452660cd98abd2fa88a92ac71031d8d
YuPing0612 <gisellewang@gms.tku.edu.tw>
2018-07-05 14:16:52 +0800

    fix run.sh bug

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-docker run -v $PWD/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -p 9200:9200 -d --name es_ik_5 elasticsearch-ik-analyzer:5.4.3
+docker run -v $PWD/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -p 9200:9200 -d --name es_ik_5 gisellewang/elasticsearch-ik-analyzer:5.4.3
 
 docker run -p 9100:9100 -d mobz/elasticsearch-head:5
